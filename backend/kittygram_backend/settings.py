bb750796f956910ea3f2274871108206361ca00f
Maniackaa <maniac_kaa@mail.ru>
2023-06-06 13:35:02 +0700

    remove dotenv

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,16 +1,23 @@
+# В общем убрал я dotenv и засунул все переменные сюда, так как яндекс
+# не принимает сним. Вот тему создал:
+# https://app.pachca.com/chats/3678643?message=57033425
 # flake8: noqa
 import os
 from pathlib import Path
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv()
+# load_dotenv()
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'jango-insecure-5!@(1#jwja4*pmu@0t2@r$!m8*g!tftwuotz4a527mqz99-u12'
 
-DEBUG = (os.getenv('DEBUG', 'False').lower() == 'true')
+# DEBUG = (os.getenv('DEBUG', 'False').lower() == 'true')
+DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['158.160.69.22', '127.0.0.1', 'localhost',
+                 'taskikiski.hopto.org']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -56,14 +63,25 @@ TEMPLATES = [
 WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('POSTGRES_DB', 'django'),
+#         'USER': os.getenv('POSTGRES_USER', 'django'),
+#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+#         'HOST': os.getenv('DB_HOST', ''),
+#         'PORT': os.getenv('DB_PORT', 5432)
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', 5432)
+        'NAME': 'django',
+        'USER': 'django_kitty_user',
+        'PASSWORD': '123321',
+        'HOST': 'db_kitty',
+        'PORT': '5432'
     }
 }
 
