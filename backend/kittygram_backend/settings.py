3f31571d75684c654d4a9d3f97c8f7b31ac5bb87
Maniackaa <maniac_kaa@mail.ru>
2023-06-06 15:55:21 +0700

    test

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -70,8 +70,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django_kitty_user'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', '123321'),
-        'HOST': os.getenv('DB_HOST', 'db_kitty'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
